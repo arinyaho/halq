09717675e159e18bdb9be0cfd9ce6bcbf1c0d350
damco <arinyaho@gmail.com>
2022-01-21 00:36:53 +0900

    Add reblancing calculator

--- rebalance.py
+++ rebalance.py
@@ -1,5 +1,6 @@
 import yaml
 import sys
+import math
 import pandas as pd
 from pandas_datareader import data as pdr
 import yfinance as yf
@@ -10,45 +11,46 @@ yf.pdr_override()
 
 
 def main():
-    if len(sys.argv) != 2:
-        print('Usage: port.py [yml file]')
+    if len(sys.argv) != 3:
+        print('Usage: port.py [dollar] [port yaml]')
         sys.exit(1)
 
-    f = sys.argv[1]
+    cash = int(sys.argv[1])
+    f = sys.argv[2]
     with open(f, 'r') as fin:
         try:
             port = yaml.safe_load(fin)
         except yaml.YAMLError as ex:
             print(ex)
             exit(2)
-    prices = {}
-    ssum = {}
-    strats = port['portfolio']['strategies']
+    
     today = datetime.today()
-    for sname, sport in strats.items():
-        if sname not in ssum:
-            ssum[sname] = 0
-        print(sname, sport)
-        for ticker, count in sport.items():
+
+    prices = {}
+
+    strats = port['portfolio']['strategies']
+    for sname in strats:
+        strat = strats[sname]
+        portion = float(strat['Portion']) / 100
+        scash = cash * portion
+        print(sname + ": " + "$" + str(scash))
+        res = {}
+        tickers = strat['Tickers']
+        for ticker in tickers:
+            tportion = float(tickers[ticker]) / 100
+            tcash = scash * tportion
+            if tportion == 0:
+                pass
             if ticker not in prices:
                 data = pdr.get_data_yahoo(ticker, start=today, end=today, progress=False)['Close']
                 price = data.iloc[0]
                 prices[ticker] = price
             else:
                 price = prices[ticker]
-            value = price * count
-            ssum[sname] += value
-    total = sum(list(ssum.values()))
+            tcount = math.floor(tcash / price)
+            if tcount != 0:
+                print('\t', ticker, tcount)
 
-    sportion = {}
-    for sname in strats:
-        sportion[sname] = 100. * ssum[sname] / total
-    print(sportion)
-
-    val = list(sportion.values())
-    label = list(sportion.keys())
-    plt.pie(val, labels=label, autopct='%.1f%%')
-    plt.show() 
 
 
 if __name__ == '__main__':
